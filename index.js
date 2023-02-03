1f0fa80394366491fcef3d824c776c523d4967e5
Radon222 <89600373712@mail.ru>
2023-02-04 01:14:48 +0300

    add controllers

--- index.js
+++ index.js
@@ -1,115 +1,44 @@
-import express, { response } from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
-import { validationResult } from "express-validator";
-import { registerValidation } from "./validations/auth.js";
-import UserModel from "./models/User.js";
+import express, { response } from 'express';
+
+import mongoose from 'mongoose';
+
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from './validations.js';
+
+import ckeckAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
+import checkAuth from './utils/checkAuth.js';
+import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.llbjemp.mongodb.net/blog?retryWrites=true&w=majority"
+    'mongodb+srv://admin:wwwwww@cluster0.llbjemp.mongodb.net/blog?retryWrites=true&w=majority'
   )
-  .then(() => console.log("DB OK"))
-  .catch((err) => console.log("DB ERROR", err));
+  .then(() => console.log('DB OK'))
+  .catch(err => console.log('DB ERROR', err));
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({
-        message: "Пользователь не найден",
-      });
-    }
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Неверный логин или пароль",
-      });
-    }
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-    const { passwordHash, ...userData } = user._doc;
+app.post('/auth/login', loginValidation, UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', ckeckAuth, UserController.getMe);
 
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось авторизоваться",
-    });
-  }
-});
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось зарегистрироваться",
-    });
-  }
-});
-
-app.get("auth/me", (req, res) => {
-  try {
-  } catch (err) {}
-});
-
-app.listen(4444, (err) => {
+// app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+// app.delete('/posts', PostController.remove);
+// app.patch('/posts', PostController.update);
+app.listen(4444, err => {
   if (err) {
     return console.log(err);
   }
 
-  console.log("Server OK");
+  console.log('Server OK');
 });
